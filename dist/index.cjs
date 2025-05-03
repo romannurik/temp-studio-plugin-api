7a5b428c68b719ba9697b562030f31a48d9d3331
Roman Nurik <roman@nurik.net>
2025-05-03 14:27:18 -0400

    init

---
+++ dist/index.cjs
@@ -0,0 +1,1 @@
+"use strict";function e(e){return e}exports.defineMonospacePlugin=e;
---
+++ dist/index.d.ts
@@ -0,0 +1,35 @@
+import { Agent, MaybePromise, Tool } from "./include/index-EUhzTRmZ.js";
+
+//#region src/plugin-api.d.ts
+/**
+* API to be implemented by plugins
+*/
+/**
+ * API to be implemented by plugins
+ */
+interface Plugin {
+  loadAgents?(): MaybePromise<Agent[]>;
+  loadTools?(): MaybePromise<Tool[]>;
+}
+type SupportedFeature = 'ai';
+type PackageJSONAddons = {
+  monospacePlugin: {
+    /**
+     * Semver
+     */
+    apiVersion: string;
+    features: SupportedFeature[];
+  };
+};
+/**
+ * The interface an IDX plugin is expected to return (return an object of this shape from its
+ * `activate()` function.
+ */
+interface Export {
+  monospacePlugin: Plugin;
+}
+/**
+ * Helper method for IDX plugins to export the right object shape from its `activate()` function.
+ */
+declare function defineMonospacePlugin(plugin: Plugin): Plugin; //#endregion
+export { Export, PackageJSONAddons, Plugin, SupportedFeature, defineMonospacePlugin };
